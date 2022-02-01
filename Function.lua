f376ad70eb8936ba3685a35349c05efc7989c2bd
Tiago Vargas <tiago.ce10@outlook.com>
2022-01-31 22:16:54 -0300

    refactor: Function now as a module

---
+++ Function.lua
@@ -0,0 +1,98 @@
+local Function = {}
+
+-- Function.prototype = {
+-- 	plot      = Function.Plot,
+-- 	setDomain = Function.SetDomain,
+-- 	setGraph  = Function.SetGraph,
+-- 	getCOM    = Function.GetCOM,
+-- 	printCOM  = Function.PrintCOM
+-- }
+
+--[[ Creates a new instance of `Function` ]]
+Function.New = function (exp)
+	local o  = {}
+	o.exp    = exp
+	-- setmetatable(o, Function.metatables)
+	o.plot      = Function.Plot
+	o.setDomain = Function.SetDomain
+	o.setGraph  = Function.SetGraph
+	o.getCOM    = Function.GetCOM
+	o.printCOM  = Function.PrintCOM
+	return o
+end
+
+--[[ Draws the graph ]]
+Function.Plot = function (self)
+	love.graphics.line(Function.Unpackgraph(self.graph + Origin))
+end
+
+--[[ Creates a domain of values with ends in `a` and `b`, with `numpoints` points ]]
+Function.SetDomain = function (self, a, b, numpoints)
+	self.domain = {}
+	local dx = (b-a) / (numpoints-1)
+	for i = a, b, dx do
+		table.insert(self.domain, i)
+	end
+end
+
+Function.SetGraph = function (self, fun)
+	self.graph = {}
+	for i = 1, #self.domain do
+		local x = self.domain[i]
+		self.graph[i] = {x = x, y = -fun(x, self.exp)}
+	end
+
+	setmetatable(self.graph, Function.graph_metatables)
+end
+
+Function.GetCOM = function (self)
+	local Sx, Sy, L = 0, 0, 0
+	for i = 1, #self.graph-1 do
+		local dL = dist(self.graph[i], self.graph[i+1])
+		L = L + dL
+		Sx = Sx + self.graph[i].x * dL
+		Sy = Sy + self.graph[i].y * dL
+	end
+
+	self.com = {x = Sx / L, y = Sy / L}
+end
+
+Function.PrintCOM = function (self)
+	love.graphics.circle("fill", self.com.x, self.com.y, 4)
+end
+
+
+Function.graph_metatables = {
+	__mul = function (graph, transformation)
+		for i = 1, #graph do
+			graph[i].x = graph[i].x * transformation.Lx
+			graph[i].y = graph[i].y * transformation.Ly
+		end
+		return graph
+	end,
+
+	__add = function (graph, point)
+		for i = 1, #graph do
+			graph[i].x = graph[i].x + point.x
+			graph[i].y = graph[i].y + point.y
+		end
+		return graph
+	end
+}
+
+-- Function.metatables = {
+-- 	__index = Function.prototype
+-- }
+
+--[[ unpacks set of points into a sequence {x1, y1, x2, y2, ...} ]]
+Function.Unpackgraph = function (graph)
+	local coordinates = {}
+	for i = 1, #graph do
+		table.insert(coordinates, graph[i].x)
+		table.insert(coordinates, graph[i].y)
+	end
+
+	return coordinates
+end
+
+return Function
--- main.lua
+++ main.lua
@@ -1,134 +1,20 @@
 -- Exp = "(x-1)^2/2"
+local Function = require("Function")
+
+-- Domain = {}
+-- Graph = {}
 Origin = {}
-Domain = {}
-Graph = {}
 Scale = {}
 
 Viewport = {}
 
-Default = {
-	-- color
-}
-
-local function unpackgraph(graph)
-	--[[ unpacks set of points into a sequence {x1, y1, x2, y2, ...} ]]
-	local coordinates = {}
-	for i = 1, #graph do
-		table.insert(coordinates, graph[i].x)
-		table.insert(coordinates, graph[i].y)
-	end
-
-	return coordinates
-end
-
-local function dist(P, Q)
-	return math.sqrt( (P.x - Q.x)^2 + (P.y - Q.y)^2 )
-end
-
--- metatables = {
--- 	__index = function (table, key)
--- 		return Function.prototype[key]
--- 	end
+-- Default = {
+-- 	-- color
 -- }
 
--------------------------------------------------------------------------------
---[[ NAMESPACE ]]
-Function = {}
---[[
-	expression
-	domain
-	graph
-	color
-	mode
-	thickness
-	c.o.m.
-	roots
-]]
 
-
-
-Function.graph_metatables = {
-	__mul = function (graph, transformation)
-		for i = 1, #graph do
-			graph[i].x = graph[i].x * transformation.Lx
-			graph[i].y = graph[i].y * transformation.Ly
-		end
-		return graph
-	end,
-
-	__add = function (graph, point)
-		for i = 1, #graph do
-			graph[i].x = graph[i].x + point.x
-			graph[i].y = graph[i].y + point.y
-		end
-		return graph
-	end
-}
-
-
---[[ Draws the graph ]]
-Function.Plot = function (self)
-	love.graphics.line(unpackgraph(self.graph + Origin))
-end
-
---[[ Creates a domain of values with ends in `a` and `b`, with `numpoints` points ]]
-Function.SetDomain = function (self, a, b, numpoints)
-	self.domain = {}
-	local dx = (b-a) / (numpoints-1)
-	for i = a, b, dx do
-		table.insert(self.domain, i)
-	end
-end
-
-Function.SetGraph = function (self, fun)
-	self.graph = {}
-	for i = 1, #self.domain do
-		local x = self.domain[i]
-		self.graph[i] = {x = x, y = -fun(x, self.exp)}
-	end
-
-	setmetatable(self.graph, Function.graph_metatables)
-end
-
-Function.GetCOM = function (self)
-	local Sx, Sy, L = 0, 0, 0
-	for i = 1, #self.graph-1 do
-		local dL = dist(self.graph[i], self.graph[i+1])
-		L = L + dL
-		Sx = Sx + self.graph[i].x * dL
-		Sy = Sy + self.graph[i].y * dL
-	end
-
-	self.com = {x = Sx / L, y = Sy / L}
-end
-
-Function.PrintCOM = function (self)
-	love.graphics.circle("fill", self.com.x, self.com.y, 4)
-end
-
-Function.prototype = {
-	plot      = Function.Plot,
-	setDomain = Function.SetDomain,
-	setGraph  = Function.SetGraph,
-	getCOM    = Function.GetCOM,
-	printCOM  = Function.PrintCOM
-}
-
-Function.metatables = {
-	__index = Function.prototype
-}
-
---[[ Creates a new instance of `Function` ]]
-Function.New = function (exp)
-	local o  = {}
-	o.exp    = exp
-	setmetatable(o, metatables)
-	-- o.plot      = Function.Plot
-	-- o.setDomain = Function.SetDomain
-	-- o.setGraph  = Function.SetGraph
-	-- o.getCOM    = Function.GetCOM
-	-- o.printCOM  = Function.PrintCOM
-	return o
+function dist(P, Q)
+	return math.sqrt( (P.x - Q.x)^2 + (P.y - Q.y)^2 )
 end
 
 
@@ -149,10 +35,10 @@ function love.load(args)
 	Origin = {x = Viewport.width/2, y = Viewport.height/2}
 	Scale = {Lx = 50, Ly = 50}
 
-	-- Domain = Function.SetDomain(-3, 3, 600)
 	f = Function.New()
 	f.exp = "(x-1)^2/2"
 	f:setDomain(-3, 3, 600)
+	-- f:getCOM()
 
 	d = 4
 end
@@ -160,6 +46,7 @@ end
 function love.update(dt)
 	f:setGraph(F)
 	f.graph = f.graph * Scale
+	-- f:getCOM() -- doesn't work here, but compiles...
 
 	if love.keyboard.isDown("up") then
 		Origin.y = Origin.y + d
@@ -175,7 +62,6 @@ function love.update(dt)
 end
 
 function love.draw()
-	-- love.graphics.setLineWidth(1)
 	drawaxis({x = Origin.x, y = Origin.y}, Viewport.width, Viewport.height)
 	f:plot()
 	f:getCOM()
@@ -183,8 +69,6 @@ function love.draw()
 end
 
 function love.keypressed(key)
-	-- print(key)
-
 	if key == "\\" and love.keyboard.isDown("rctrl") then
 		debug.debug()
 	end
